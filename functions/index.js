678deed05668cd14a2458970f0bfb9cf658ebb9e
Unknown <conor@wilxite.com>
2019-03-03 20:48:13 +0000

    call to get articles from firebase and load index view

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,16 @@
 const functions = require('firebase-functions')
+const firebase = require('firebase-admin')
 const express = require('express')
 const engines = require('consolidate')
+
+const firebaseApp = firebase.initializeApp(
+    functions.config().firebase
+)
+
+function getArticles() {
+    const ref = firebaseApp.database().ref('articles')
+    return ref.once('value').then(snap => snap.val())
+}
 
 const app = express()
 app.engine('hbs', engines.handlebars)
@@ -9,8 +19,10 @@ app.set('view engine', 'hbs')
 
 app.get('/', (request, response) => {
     response.set('Cache-Control', 'public, max-age=300, s-maxage=600') // for caching on nearest content delivery network node
-    response.render('index', {
-        articles
+    getArticles().then(articles => {
+        response.render('index', {
+            articles
+        })
     })
 })
 
