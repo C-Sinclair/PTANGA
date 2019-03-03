f004d63f99f600c28a58189798662eaa3ce5ef66
Unknown <conor@wilxite.com>
2019-03-03 20:41:13 +0000

    use handlebars as template engine

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,17 @@
 const functions = require('firebase-functions')
 const express = require('express')
+const engines = require('consolidate')
 
 const app = express()
+app.engine('hbs', engines.handlebars)
+app.set('views', './views')
+app.set('view engine', 'hbs')
 
-app.get('/timestamp', (request, response) => {
-    response.send(`${Date.now()}`)
-})
-
-app.get('/timestamp-cached', (request, response) => {
-    response.set('Cache-Control', 'public, max-age=300, s-maxage=600')
-    response.send(`${Date.now()}`)
+app.get('/', (request, response) => {
+    response.set('Cache-Control', 'public, max-age=300, s-maxage=600') // for caching on nearest content delivery network node
+    response.render('index', {
+        articles
+    })
 })
 
 exports.app = functions.https.onRequest(app)
